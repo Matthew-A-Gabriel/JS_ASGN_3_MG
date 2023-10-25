82153af6dab69bb5ed292f759b8a7292b54168ea
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-25 08:57:53 -0700

    Finished

--- Array.js
+++ Array.js
@@ -1,53 +1,21 @@
-// // Objectives:
-
-// Students will learn to use for loops with arrays.
-// Students will experiment with different approaches to implements JavaScript array methods from scratch.
-// Students will reuse created functions in order to complete tasks of other functions.
-// Students will learn array implementations.
-// Assignment:
-
-// Now that we have covered Arrays and Array Methods, we see how useful arrays can be. The methods that come along with arrays make them a great tool to store and manipulate data. Your new JavaScript assignment will consist of you creating the following Array functions from scratch:
-
-// push
-// pop
-// toString
-// join
-// concat
-// When creating these functions you are only allowed to use array.length so you can find the length of the array. To clear, you can not use the functions above. pretend they do not exist and you have to create them.
-
-// Submit the link to your GitHub repo to complete this assignment!
-
- 
-
-// Notes to get you started:
-
-// Create and HTML file, add the starter HTML code, and link your script. You do NOT need to create a CSS file.
-// If you want to test if your code is working, use Inspect Element and click on "Console" if you want to see things you print.
-// Make sure you add comments to your code so that you know what is going on.
-// Reasons why I will remove points:
-
-// Assignment is late
-// Using the pre made array methods
-// No comments on your JavaScript code
-// You did not create all the functions
-// There is an error in your code
-// You did not use Git when uploading your code to GitHub
-
-// Array
+// Arrays
 const array_stuff = ["item1","item2","item3",]
+const array_2_part_A =["a1","a2","a3"]
+const array_2_part_B = ["b1","b2","b3"]
+
 
 // Does functions and logs in console
-console.log("LOG : " + array_stuff)
+console.log("Starting - LOG : " + array_stuff)
 pushed("item4")
-console.log("LOG : " + array_stuff)
+console.log("Push - LOG : " + array_stuff)
 poped()
-console.log("LOG : " + array_stuff)
-
-console.log("LOG : " + array_stuff)
-
-console.log("LOG : " + array_stuff)
-
-console.log("LOG : " + array_stuff)
+console.log("Pop - LOG : " + array_stuff)
+toStringed()
+console.log("toString - LOG : " + toStringed())
+joined()
+console.log("joined - LOG : " + joined())
+concated()
+console.log("concated - LOG : " + concated())
 
 
 // add Item4
@@ -57,29 +25,38 @@ function pushed (item4) {
 
 // remove last 2 items
 function poped (removeItem) {
-    for (var i = 0;  i<2; i++) {
+    for (var i = 0;  i<1; i++) {
         array_stuff.length = array_stuff.length - 1
     }
 }
 
-// 
+// toString but it's manual... we grab the values of array_stuff, and put them 
 function toStringed () {
-
-
-
+    var divider = ""
+    for (var i = 0; i < array_stuff.length; i++) {
+        if(i > 0) {
+            divider += ',';
+        }
+        divider += array_stuff[i];
+    }
+return divider;
 }
+ 
 
-// 
+// it's toString, but with "/" instead of "," 
 function joined () {
-
-
-
+    var divider = ""
+    for (var i = 0; i < array_stuff.length; i++) {
+        if(i > 0) {
+            divider += '/';
+        }
+        divider += array_stuff[i];
+    }
+return divider;
 }
 
-// 
+// adds part A and part B together to get the 2nd array.
 function concated () {
-
-
-
+    var array_stuff_2 = array_2_part_A + "," +array_2_part_B
+    return array_stuff_2
 }
-
